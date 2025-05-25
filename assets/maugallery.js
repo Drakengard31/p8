b94799fbb244ad9182fcbb4c561d270af6fe701d
Drakengard31 <lucdimitri31@gmail.com>
2025-05-25 19:00:08 +0200

    compression des images, mettre des balise manquantes attributs alt a des images et enlever un h1 pour le mettre en balise <p>

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
 (function($) {
   $.fn.mauGallery = function(options) {
     var options = $.extend($.fn.mauGallery.defaults, options);
@@ -261,3 +281,4 @@
     }
   };
 })(jQuery);
+
